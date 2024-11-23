9cde3e9c7c0183cc33390293db705ac0f81d4e9c
Blazin Ammo <nicktoddolio@gmail.com>
2024-11-23 11:11:34 -0800

    nopee

--- public/game.js
+++ public/game.js
@@ -175,6 +175,6 @@ socket.on('environment', (data) => {
 });
 
 // Update player position
-setInterval(updatePosition, 200); // Roughly 60 fps
+setInterval(updatePosition, 16); // Roughly 60 fps
 // Update every 100ms to ensure smooth updates as the player moves
 setInterval(sendViewportUpdate, 200);
